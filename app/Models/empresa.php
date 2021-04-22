229cac09410b7c87ae49dff6b713639c97c3d426
luisRJimenez <luisrodrigojimenez@gmail.com>
2021-04-22 16:15:36 -0500

    primer comit

---
+++ app/Models/empresa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class empresa extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'empnombre', 'empdireccion', 'emptelefono', 'empestado', 'municipio_id', 'departamento_id'
+    ];
+
+
+
+
+
+}
